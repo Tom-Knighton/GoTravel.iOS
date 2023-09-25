d3f96723a481c20e2f7128469fcd144793dba5e7
Tom K <tomknighton@icloud.com>
2023-09-25 22:29:07 +0100

    Initial Commit

---
+++ GoLondon/GoLondonApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoLondonApp.swift
+//  GoLondon
+//
+//  Created by Tom Knighton on 25/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct GoLondonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
